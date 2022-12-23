53f2f9a16564c68ea3d8891c27cb3a17a6cdb28a
ArjunSajeev00 <arjun.sajeev@experionglobal.com>
2022-12-23 17:19:22 +0530

    Login and Google Login

--- ecommerce-app/src/components/Navbar.tsx
+++ ecommerce-app/src/components/Navbar.tsx
@@ -9,38 +9,52 @@ import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
+import { Divider } from '@mui/material';
+import { Button, MenuList } from '@mui/material';
+import Modal from '@mui/material/Modal';
+import GoogleLogin from 'react-google-login';
+const responseGoogle = (response: any) => {
+  console.log(response);
+}
 
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  height: 200,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
-const settings = ['My Profile', 'Wishlist', 'Cart', 'Login','Logout'];
-
-function NavBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+function Navbar() {
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
 
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElUser(event.currentTarget);
   };
 
-
-
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
-
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+//testing for github
   return (
-
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
           </Box>
+
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar/>
+                <Avatar />
               </IconButton>
             </Tooltip>
             <Menu
@@ -51,15 +65,41 @@ function NavBar() {
                 vertical: 'top',
                 horizontal: 'right',
               }}
-             
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
+              <MenuItem onClick={handleCloseUserMenu}>
+              <MenuList>
+              <Typography><Button>My Profile</Button></Typography>
+              <Typography><Button>Wishlist</Button></Typography>
+              <Typography><Button>Cart</Button><Divider /></Typography>
+             
+              <Button onClick={handleOpen}>Login</Button>
+              <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+              >
+              <Box sx={style}>
+             
+              <GoogleLogin
+              clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+              buttonText="LOGIN WITH GOOGLE"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={'single_host_origin'}
+              />
+              </Box>
+              </Modal>
+              <Typography><Button>Logout</Button></Typography>
+              </MenuList>
+              </MenuItem>
             </Menu>
           </Box>
         </Toolbar>
@@ -67,4 +107,4 @@ function NavBar() {
     </AppBar>
   );
 }
-export default NavBar;
+export default Navbar;
